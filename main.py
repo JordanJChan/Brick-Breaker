493cb1c5a9fa5f8950a81a484c1f760a3f16476c
Jordan Chan <j.chan52@share.epsb.ca>
2025-03-04 13:51:16 -0700

    added some comments

--- main.py
+++ main.py
@@ -4,47 +4,51 @@ author: Jordan Chan
 date: 2025-2-6
 """
 
-
 import pygame
-from random import randint
-import time
+from random import randint # Use to pick a random number
+import time # Use to keep track of the time
 
 class Window:
-
+    """
+    Class to make the game window
+    """
     def __init__(self, title, width, height, fps):
-        self.__title = title
+        self.__title = title # Window name
         self.__fps = fps
-        self.__width = width
-        self.__height = height
-        self.__screen_dimensions = (self.__width, self.__height)
+        self.__width = width # Window width
+        self.__height = height # Window height
+        self.__screen_dimensions = (self.__width, self.__height) # # Window dimensions
         self.__Clock = pygame.time.Clock()
         self.__surface = pygame.display.set_mode(self.__screen_dimensions)
-        self.__surface.fill((128, 128, 128))
+        self.__surface.fill((128, 128, 128)) # Make the background color gray
         pygame.display.set_caption(self.__title)
 
 
-    def clear_screen(self):
+    def clear_screen(self): #  Make the screen gray
         self.__surface.fill((128, 128, 128))
 
 
-    def update_frame(self):
+    def update_frame(self): # Change the frame of the window
         self.__Clock.tick(self.__fps)
         pygame.display.flip()
 
 
-    def get_surface(self):
+    def get_surface(self): # Get the surface of the window
         return self.__surface
 
 
     def get_width(self):
-        return self.__width
+        return self.__width # Returns the width of the window
 
 
     def get_height(self):
-        return self.__height
+        return self.__height # Returns the height of the window
 
 
 class mySprite:
+    """
+    Template to create sprites
+    """
 
     def __init__(self, width=1, height=1, color=(255, 255, 255), x=0, y=0, speed=8):
         self.__width = width
@@ -134,7 +138,6 @@ class mySprite:
     
     def make_box(self):
         return pygame.Rect(self.__x, self.__y, self.__width, self.__height)
-
 
 
 class Text(mySprite):
